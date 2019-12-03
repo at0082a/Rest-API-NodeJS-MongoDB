1650bf69b0454fa925c24df8ad27de625731110d
Alex Tilatti <alex.tilatti@gmail.com>
2019-12-03 10:53:09 -0500

    edited file routes

--- routes/toDoList.js
+++ routes/toDoList.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Item = require("../schema");
+const Item = require("../models/schema");
 
 //get all items
 router.get("/items", (req, res) => {
