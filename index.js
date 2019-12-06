38c340ab68cba0be19f2a75eb9eabaf45046d21f
Alex Tilatti <alex.tilatti@gmail.com>
2019-12-05 23:54:53 -0500

    final edits

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const logger = require("morgan");
 const port = process.env.PORT || 3001;
 
 mongoose.connect('mongodb://localhost:27017/todolistitems', {useNewUrlParser: true, useUnifiedTopology: true});
